f3ad1312199598f2ef53882eaca6788a8dff6f34
Abagail Hudson <abagailanne@gmail.com>
2019-05-19 17:06:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-
   BRANDS = []
+  
   def initialize(brand)
     @brand = brand
   end
@@ -13,8 +13,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end
 end
